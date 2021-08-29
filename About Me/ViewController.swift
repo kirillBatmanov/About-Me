68ee3ec7ea293233b7485caaf1f2d7ea59aff14a
Kirill Batmanov <BatmanovKS@icloud.com>
2021-08-29 13:35:48 +0200

    Initial Commit

---
+++ About Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Кирилл Батманов on 29.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
